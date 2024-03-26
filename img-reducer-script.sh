5aa8cc85a23f391bbac03510e3e9141938c031bc
Ajnur Ademi <Ajnur.Ademi@ksb-sg.ch>
2024-03-26 11:05:01 +0100

    Updated Script - Ckeck AWS Cli (Live Share - Nicolas / Aron / Ajnur)

--- img-reducer-script.sh
+++ img-reducer-script.sh
@@ -1,79 +1,33 @@
 #!/bin/bash
 
 ################################################################################
-# Skriptname:     img-reducer-script.sh
-# Beschreibung:   Mit diesem Script können sie ein Bild, welches auf Ihrem Laptop gespeichert haben,
-#                 mittels einer Pfad angabe auswählen. Dannach wird das Bild automatisch verkleinert und sie 
-#                 können das verkleinerte Bild wieder herunterladen.  
-# Autor:          Ajnur Herbel, Aron Herbel & Hicolas Haas
-# Datum:          19. March 2024
-# Version:        1.0
+# Script Name:    img-reducer-script.sh
+# Description:    With this script, you can select an image stored on your laptop
+#                 using a path. Afterwards, the image will be automatically resized and 
+#                 you can download the resized image again.
+# Author:         Ajnur Ademi | Aron Herbel | Nicolas Haas
+# Date:           26. March 2024
+# Version:        1.3
 ################################################################################
 
-# Bucket-Namen definieren
-demo_users_images_bucket="demo-users-images-bucket"
-demo_user_thumbnails_bucket="demo-user-thumbnails-bucket"
-
-# Bucket erstellen - Demo Users Images
-aws s3 mb s3://$demo_users_images_bucket
-
-# Bucket erstellen - Demo User Thumbnails
-aws s3 mb s3://$demo_user_thumbnails_bucket
-
-echo "Buckets wurden erfolgreich erstellt:"
-echo "- $demo_users_images_bucket"
-echo "- $demo_user_thumbnails_bucket"
-
-# Funktion zum Hochladen eines Bildes
-upload_image() {
-    local image_path="$1"
-    local image_name="$(basename "$image_path")"
-    local destination="s3://$demo_users_images_bucket/$image_name"
-    
-    # Bild hochladen
-    aws s3 cp "$image_path" "$destination"
-
-    echo "Bild erfolgreich hochgeladen nach $destination"
-}
-
-# Benutzer nach Pfad zu einem PNG-Bild fragen
-read -p "Geben Sie den Pfad zu einem PNG-Bild ein, das hochgeladen werden soll: " image_path
-
-# Überprüfen, ob die Datei existiert und ein PNG-Bild ist
-if [ -f "$image_path" ] && [[ "$image_path" == *.png ]]; then
-    upload_image "$image_path"
+# Check if aws CLI is installed
+if command -v aws &> /dev/null
+then
+    echo "aws-cli is already installed"
 else
-    echo "Ungültige Datei oder Dateityp. Bitte geben Sie einen gültigen Pfad zu einer PNG-Datei ein."
+    echo "aws-cli is installing ...."
+
+    # Install AWS CLI
+    sudo apt-get update
+    sudo apt-get install -y awscli
+
+    # Check if installation was successful
+    if command -v aws &> /dev/null
+    then
+        echo "aws-cli installed successfully"
+    else
+        echo "Error by downloading aws-cli"
+        exit 1
+    fi
 fi
-
-#
-
-
-
-
-
-
-
-# Funktion zum Herunterladen eines verkleinerten Bildes
-download_thumbnail() {
-    local image_name="$1"
-    local destination="./$image_name-thumbnail.png"
-    local source="s3://$demo_user_thumbnails_bucket/$image_name"
-    
-    # Verkleinertes Bild herunterladen
-    aws s3 cp "$source" "$destination"
-
-    echo "Verkleinertes Bild erfolgreich heruntergeladen nach $destination"
-}
-
-# Verkleinertes Bild herunterladen
-download_thumbnail "$image_name"
-
-
-# Clean up
-echo "Lösche die Buckets..."
-aws s3 rb s3://$demo_users_images_bucket --force
-aws s3 rb s3://$demo_user_thumbnails_bucket --force
-
-echo "Buckets wurden erfolgreich gelöscht."
 
